f6567925445382efca513158b1ae8ea60fb50fcd
maybeYOLO <maybeyolo1@mail.ru>
2023-06-28 13:13:23 +0000

    Настроенный taski

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,13 +1,29 @@
 from pathlib import Path
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+# Скопируйте DSN из вашего личного кабинета на Sentry: 
+# Settings → Projects → <ваш-проект> → Client Keys (DSN).
+sentry_sdk.init(
+        # В этой переменной будет значение для вашего проекта.
+    dsn="https://42eacc7bc70f44dc88623e02f4a33206@o4505431868112896.ingest.sentry.io/4505431872045056",
+    integrations=[
+        DjangoIntegration(),
+    ],
+
+    traces_sample_rate=1.0,
+
+    send_default_pii=True
+)
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['158.160.31.143', '127.0.0.1', 'localhost', 'taskidaski.hopto.org']
 
 
 # Application definition
@@ -103,7 +119,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static_backend/'
+
+STATIC_ROOT = BASE_DIR / 'static_backend'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
